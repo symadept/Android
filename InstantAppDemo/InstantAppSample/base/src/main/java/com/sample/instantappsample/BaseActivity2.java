453f802a809097f3b1e023e777a7733bad279598
Shaik, Mustafa <Mustafa.Shaik@VerizonWireless.com>
2018-10-17 20:45:23 -0500

    Created Base+Instant along with Base+Installed

---
+++ InstantAppDemo/InstantAppSample/base/src/main/java/com/sample/instantappsample/BaseActivity2.java
@@ -0,0 +1,15 @@
+package com.sample.instantappsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sample.base.R;
+
+public class BaseActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base2);
+    }
+}
